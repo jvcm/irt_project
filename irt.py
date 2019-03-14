309c6c38cb9a4594d831ef411038991210c4686e
jvcm <jvcm@cin.ufpe.br>
2019-03-14 15:21:54 -0300

    irt regression

--- irt.py
+++ irt.py
@@ -7,6 +7,7 @@ from sklearn.tree import DecisionTreeRegressor
 from sklearn.neural_network import MLPRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor, AdaBoostRegressor
+from sklearn.preprocessing import StandardScaler
 
 class IRTModel:
 
@@ -50,23 +51,42 @@ class IRTModel:
         return
     
 def main():
-    """Função principal da aplicação.
-    """
-    data = pd.read_csv('data/SWD.csv')
-    X = data.iloc[:,:-1]
-    y = data.iloc[:, -1]
+    """Função principal da aplicação."""
+
+    names = ['mpg', 'cylinders', 'displacement', 'horsepower', 'weight', 'acceleration', 'model_year', 'origin', 'car_name']
+    data = pd.read_csv('data/UCI - 45/auto_mpg.csv')
+    data.columns = names
+    data = data.dropna()
+
+    X = data.iloc[:, 1:-3]
+    y = data.iloc[:, 0]
     
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state = 42)
+
+    cols = X_train.columns
+    sc = StandardScaler()
+    X_train = sc.fit_transform(X_train)
+    X_test = sc.transform(X_test)
+
+    # print(len(X_test))
 
     models = [LinearRegression(), BayesianRidge(), svm.SVR(kernel= 'linear'), svm.SVR(kernel = 'rbf'),\
          KNeighborsRegressor(), DecisionTreeRegressor(), RandomForestRegressor(),\
-              AdaBoostRegressor(), SGDRegressor(), MLPRegressor()]
+              AdaBoostRegressor(), MLPRegressor()]
 
     irt = IRTModel(models= models)
     irt.fit(X_train= X_train, y_train= y_train)
-    irt.irtMatrix(X_test= X_test, y_test= y_test)
+    irt.irtMatrix(X_test= X_test, y_test= y_test, normalize= True)
 
-    print(irt.irt_matrix)
+    # print(irt.irt_matrix.T)
+    irt.irt_matrix.T.to_csv(path_or_buf= './beta_irt/irt_data_mpg_s79_f20_sd42.csv', index= False, encoding='utf-8')
+    
+    X_test = pd.DataFrame(X_test, columns= cols)
+    X_test.columns = cols
+    X_test['noise'] = 0
+    
+    print(X_test.isnull().values.any(), irt.irt_matrix.isnull().values.any())
+    X_test.to_csv('./beta_irt/xtest_mpg_s79_f20_sd42.csv', index= False, encoding='utf-8')
 
 if __name__ == "__main__":
     main()
